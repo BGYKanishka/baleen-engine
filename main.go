5a4b574cddf4984c5e659827d903bbb99076088b
BGYKanishka <bgykanishka@gmail.com>
2026-05-06 21:47:11 +0530

    feat: export Docker image as tar

--- main.go
+++ main.go
@@ -3,17 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 
-	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 )
 
 func main() {
 	fmt.Println("Starting Baleen Engine (Go Edition)...")
 
+	// Create Baleen temp directory.
+	baleenDir, err := setupBaleenDirectory()
+	if err != nil {
+		panic(fmt.Errorf("failed to setup directories: %w", err))
+	}
+
 	// Connect to the local docker daemon
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-
 	if err != nil {
 		panic(err)
 	}
@@ -24,18 +32,72 @@ func main() {
 		}
 	}(cli)
 
-	// list of images
-	images, err := cli.ImageList(context.Background(), image.ListOptions{})
+	// Docker image to export.
+	targetImage := "alpine:latest"
+	fmt.Printf("Preparing to export '%s'...\n", targetImage)
+
+	if err := exportImage(cli, targetImage, baleenDir); err != nil {
+		fmt.Printf("Export failed: %v\n", err)
+		return
+	}
+
+	fmt.Println("Export complete!")
+}
+
+// creates ~/.baleen/temp and returns its path.
+func setupBaleenDirectory() (string, error) {
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	fmt.Printf("Connected to Docker! Found %d images on your Mac.\n", len(images))
+	baleenTempDir := filepath.Join(homeDir, ".baleen", "temp")
 
-	for _, img := range images {
-		if len(img.RepoTags) > 0 {
-			sizeInMB := img.Size / 1024 / 1024
-			fmt.Printf("- %s (Size: %d MB)\n", img.RepoTags[0], sizeInMB)
+	if err := os.MkdirAll(baleenTempDir, 0755); err != nil {
+		return "", err
+	}
+
+	return baleenTempDir, nil
+}
+
+// saves a Docker image as a tar file
+func exportImage(cli *client.Client, imageName string, exportDir string) error {
+	ctx := context.Background()
+
+	// Request the image tar stream from Docker.
+	imageStream, err := cli.ImageSave(ctx, []string{imageName})
+	if err != nil {
+		return err
+	}
+	defer func(imageStream io.ReadCloser) {
+		err := imageStream.Close()
+		if err != nil {
+
 		}
+	}(imageStream)
+
+	// Convert image name into a safe file name.
+	safeFilename := strings.ReplaceAll(imageName, ":", "_") + ".tar"
+	targetPath := filepath.Join(exportDir, safeFilename)
+
+	// Create the output tar file.
+	outFile, err := os.Create(targetPath)
+	if err != nil {
+		return err
 	}
+	defer func(outFile *os.File) {
+		err := outFile.Close()
+		if err != nil {
+
+		}
+	}(outFile)
+
+	fmt.Printf("Streaming image to disk at: %s\n", targetPath)
+
+	// Copy Docker's image stream directly to disk.
+	if _, err := io.Copy(outFile, imageStream); err != nil {
+		return err
+	}
+
+	return nil
 }
